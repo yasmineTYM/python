04fc348f66be88515032b1f5c88e2766469d63e7
CallmeSuper <244367691@qq.com>
2017-05-19 10:52:46 +0800

    Update codecademy-python-list & dictionary.py

--- codecademy-python-list & dictionary.py
+++ codecademy-python-list & dictionary.py
@@ -145,3 +145,134 @@ inventory['backpack'].sort()
 inventory['backpack'].remove('dagger')
 inventory['gold']=550
 
+#-------------------------------------------------------------------
+"""
+Define a function called print_list that has one argument called x.
+Inside that function, print out each element one by one. Use the existing code as a scaffold.
+Then call your function with the argument n.
+?
+
+"""
+n = [3, 5, 7]
+
+def print_list(x):
+    for i in range(0, len(x)):
+        print x[i]
+    
+print_list(n)
+
+#-------------------------------------------------------------------
+"""
+Create a function called double_list that takes a single argument x (which will be a list) and multiplies each element by 2 and returns that list. Use the existing code as a scaffold.
+"""
+
+n = [3, 5, 7]
+
+# Don't forget to return your new list!
+def double_list(x):
+    for i in range(0, len(x)):
+        x[i] = x[i]*2
+    return x
+print double_list(n)
+#-------------------------------------------------------------------
+"""
+On line 6, replace the ____ with a range() that returns a list containing [0, 1, 2].
+"""
+
+def my_function(x):
+    for i in range(0, len(x)):
+        x[i] = x[i] * 2
+    return x
+
+print my_function(range(3)) # Add your range between the parentheses!
+#-------------------------------------------------------------------
+"""
+Create a function called total that adds up all the elements of an arbitrary list and returns that count, using the existing code as a hint. Use a for loop so it can be used for any size list.
+"""
+n = [3, 5, 7]
+
+def total(numbers):
+    result=0
+    for item in numbers:
+        result+=item 
+    return result
+#-------------------------------------------------------------------    
+
+n = ["Michael", "Lieberman"]
+def join_strings(words):
+    result = []
+    for item in words:
+        result.append(item)
+    return "".join(result)
+
+print join_strings(n)
+#output:MichaelLieberman
+
+n = ["Michael", "Lieberman"]
+def join_strings(words):
+    result = []
+    for item in words:
+        result.append(item)
+    return result
+
+print join_strings(n)
+#output:['Michael', 'Lieberman']
+#-------------------------------------------------------------------
+m = [1, 2, 3]
+n = [4, 5, 6]
+
+# Add your code here!
+def join_lists(x,y):
+    return x+y
+
+
+
+
+print join_lists(m, n)
+# You want this to print [1, 2, 3, 4, 5, 6]
+#-------------------------------------------------------------------
+n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
+# Add your function here
+def flatten(lists):
+    results=[]
+    for numbers in lists:
+        for number in numbers:
+            results.append(number)
+    return results
+
+
+
+print flatten(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
